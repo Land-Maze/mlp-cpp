7c343437aab8aa97aabf4ac71511037a314e13c5
Land_Maze <61432929+Land-Maze@users.noreply.github.com>
2026-08-08 21:15:51 +0200

    feat: add transposed A multiplication

--- include/lml/matrix.hpp
+++ include/lml/matrix.hpp
@@ -33,5 +33,22 @@ namespace LML{
 
             return C;
         }
+
+        static Matrix mul_transposed_A(const Matrix& A, const Matrix& B){
+            assert(A.cols == B.rows);
+
+            Matrix C(A.rows, B.cols);
+
+            // FIXME: Optimize the naïve aproach
+            for(size_t i = 0; i < C.rows; i++){
+                for(size_t j = 0; j < C.cols; j++){
+                    for(size_t m = 0; m < A.cols; m++){
+                        C(i,j) += A(m, i) * B(m, j);
+                    }
+                }
+            }
+
+            return C;
+        }
     };
 }
