7e26735f2e4d2d8f28e36439fc309abe6aa5f6e6
Land_Maze <61432929+Land-Maze@users.noreply.github.com>
2026-08-12 19:30:44 +0200

    fix: explicit typings, fixed seed

--- src/initializer.cpp
+++ src/initializer.cpp
@@ -3,13 +3,13 @@
 
 using namespace LML;
 
-std::mt19937& Initializer::get_rng(size_t seed){
-    static std::mt19937 gen(seed);
+std::mt19937& Initializer::get_rng(){
+    static std::mt19937 gen(1337);
         return gen;
 }
 
 void Initializer::glorot_uniform(Matrix& W, size_t n_in, size_t n_out){
-    float stddev = std::sqrt(6 / static_cast<float>(n_in + n_out));
+    float stddev = std::sqrt(6.0f / static_cast<float>(n_in + n_out));
     std::uniform_real_distribution<float> dist(-stddev, stddev);
     for (auto& val : W.data) {
         val = dist(get_rng());
@@ -17,7 +17,7 @@ void Initializer::glorot_uniform(Matrix& W, size_t n_in, size_t n_out){
 }
 
 void Initializer::he_normal(Matrix& W, size_t n_in){
-    float stddev = std::sqrt(2 / static_cast<float>(n_in));
+    float stddev = std::sqrt(2.0f / static_cast<float>(n_in));
     std::normal_distribution<float> dist(0.0f, stddev);
         for (auto& val : W.data) {
             val = dist(get_rng());
