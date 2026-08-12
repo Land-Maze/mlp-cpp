2294700db3bfad8a8dc1ea6c14d87fb263bb714a
Land_Maze <61432929+Land-Maze@users.noreply.github.com>
2026-08-12 21:36:33 +0200

    feat: implement forward/backward pass with update weights method

---
+++ src/layer.cpp
@@ -0,0 +1,36 @@
+#include <lml/layer.hpp>
+#include <lml/initializer.hpp>
+
+using namespace LML;
+
+Layer::Layer(size_t n_in, size_t n_out, ActivationType method)
+: z(n_out, 1), W(n_out, n_in), b(n_out, 1), act(method), a(n_in, 1)
+, dW(n_in, 1), delta(n_in, 1), dX(n_in, 1), dZ(n_out, 1)
+{
+    Initializer::glorot_uniform(this->W, n_in, n_out);
+    Initializer::fill_zeros(this->b);
+}
+
+const Matrix& Layer::forward_pass(const Matrix &a)
+{
+    assert_dim(W, a);
+    z = Matrix::mul(this->W, a);
+
+    z = Matrix::add(z, b);
+
+    this->a = a;
+    return this->z;
+}
+
+const Matrix& Layer::backward_pass(const Matrix& d_next){
+    Activation::apply_derivative(z, dZ, this->act);
+    this->delta = Matrix::haamard_product(d_next, dZ);
+    dW = Matrix::mul_transposed_B(this->delta, this->a);
+    dX = Matrix::mul_transposed_A(this->W, this->delta);
+    return dX;
+}
+
+void Layer::update_weights(const float learning_rate){
+    Matrix coeficient(W.rows, W.cols, learning_rate);
+    this->W = Matrix::sub(W, Matrix::haamard_product(coeficient, W));
+}
