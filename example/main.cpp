4f036f0f47d9d29b551a81a10c96420b4cbbb1fe
Land_Maze <61432929+Land-Maze@users.noreply.github.com>
2026-08-18 20:20:57 +0200

    feat: change activation method in main example
    Hollyy, it started doing muuch better

--- example/main.cpp
+++ example/main.cpp
@@ -1,51 +1,50 @@
-#include <iostream>
 #include <lml/mlp.hpp>
+#include <printf.h>
 
 using namespace LML;
 
 int main() {
-    MLPCreateInfo createInfo{
-        .layer_sizes{{2,5},{5, 1}},
-        .activation_function = ActivationType::Sigmoid,
-        .learning_rate = 0.5f
-    };
+	MLPCreateInfo createInfo{};
+	createInfo.layer_sizes = {{2, 5}, {5, 1}};
+	createInfo.activation_function = ActivationType::LeakyReLu;
+	createInfo.learning_rate = 0.5f;
 
-    MLP xor_mlp(createInfo);
+	MLP xor_mlp(createInfo);
 
-    std::vector<std::pair<Matrix, Matrix>> dataset;
-    for(size_t i = 0; i < 4; i++)
-        dataset.push_back({Matrix(2,1), Matrix(1,1)});
+	std::vector<std::pair<Matrix, Matrix>> dataset;
+	for (size_t i = 0; i < 4; i++)
+		dataset.push_back({Matrix(2, 1), Matrix(1, 1)});
 
-    dataset[0].first.data = {1,0};
-    dataset[0].second.data = {1};
+	dataset[0].first.data = {1, 0};
+	dataset[0].second.data = {1};
 
-    dataset[1].first.data = {0,1};
-    dataset[1].second.data = {1};
+	dataset[1].first.data = {0, 1};
+	dataset[1].second.data = {1};
 
-    dataset[2].first.data = {1,1};
-    dataset[2].second.data = {0};
+	dataset[2].first.data = {1, 1};
+	dataset[2].second.data = {0};
 
-    dataset[3].first.data = {0,0};
-    dataset[3].second.data = {0};
+	dataset[3].first.data = {0, 0};
+	dataset[3].second.data = {0};
 
-    printf("Training phase\n");
+	printf("Training phase\n");
 
-    constexpr size_t EPOCH = 5000;
+	constexpr size_t EPOCH = 5000;
 
-    for(size_t i = 0; i < EPOCH; i++){
-        float mse = 0.0f;
-        for(auto& entry : dataset){
-            xor_mlp.forward(entry.first);
-            xor_mlp.backward(entry.second);
-            xor_mlp.update();
-            mse = xor_mlp.mse(entry.second)(0,0);
-        }
-        if((i % 1000) == 0)
-                printf("Epoch %zu, MSE %f\n", i, mse);
-    }
+	for (size_t i = 0; i < EPOCH; i++) {
+		float mse = 0.0f;
+		for (auto &entry : dataset) {
+			xor_mlp.forward(entry.first);
+			xor_mlp.backward(entry.second);
+			xor_mlp.update();
+			mse = xor_mlp.mse(entry.second)(0, 0);
+		}
+		if ((i % 1000) == 0)
+			printf("Epoch %zu, MSE %f\n", i, mse);
+	}
 
-    for(auto& entry : dataset){
-        auto result = xor_mlp.forward(entry.first);
-        printf("Input {%f, %f}, Output %f\n", entry.first(0,0), entry.first(1,0), result(0,0));
-    }
+	for (auto &entry : dataset) {
+		auto result = xor_mlp.forward(entry.first);
+		printf("Input {%f, %f}, Output %f\n", entry.first(0, 0), entry.first(1, 0), result(0, 0));
+	}
 }
